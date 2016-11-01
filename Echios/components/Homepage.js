e6c74e79635ac9c8b597bcab556543ae249f6894
Matt Gershowitz <mattgershowitz1@gmail.com>
2016-11-01 16:38:38 -0400

    constructing homepage and separated style sheet

---
+++ Echios/components/Homepage.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { View, Text, Image } from 'react-native';
+import { Container } from 'native-base';
+import Button from 'react-native-button';
+
+import styles from './styles.js'
+
+const neelPic = 'https://i.imgur.com/jzpjBY0.jpg'
+
+export default class Homepage extends Component {
+  handlePress(event) {
+    console.log('Pressed!');
+  }
+  render() {
+    return (
+      <Container>
+        <View style={styles.container}>
+          <Text style={styles.title}>
+            Echios
+          </Text>
+          <Image
+            source={{uri: neelPic}}
+            style={styles.pics}
+           />
+          <Text style={styles.instructions}>
+            Click below to start your Assesment
+          </Text>
+        <Button
+          style={{borderWidth: 1, borderColor: 'blue'}}
+          onPress={this.handlePress.bind(this)}>
+          Press Me!
+        </Button>
+
+        </View>
+      </Container>
+    );
+  }
+}
---
+++ Echios/components/styles.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    // backgroundImage: 'https://i.imgur.com/nZT3soB.jpg',
+  },
+  title: {
+    fontSize: 40,
+    textAlign: 'center',
+    color: '#ffffff',
+    fontFamily: 'helvetica',
+    margin: 50,
+    top: 0,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#ffffff',
+    margin: 5,
+  },
+  pics: {
+    marginTop: 30,
+    width: 500,
+    height: 300,
+  }
+})
